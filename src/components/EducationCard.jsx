20b2c7f48539be9f329bb04765b7f827600c88d9
Wuhib Mezemir <86829490+wuhibm@users.noreply.github.com>
2025-09-16 17:09:36 -0400

    Added all of the sections for education

--- src/components/EducationCard.jsx
+++ src/components/EducationCard.jsx
@@ -2,14 +2,19 @@ import "../styles/EducationCard.css"
 
 export default function EducationCard(props) {
     return (
-        <div className="education-card">
+        <section className="education-card">
             <h1 className="education-title">{props.title}</h1>
             <h2 className="education-degree">{props.degree}</h2>
+            {props.gpa && <h4>GPA: {props.gpa}</h4>}
             <img src={props.src} alt={props.alt} className="education-logo" />
             <p className="education-body">{props.children}</p>
-            <ul className="education-skills">
-                {props.skills.map(skill => <li className="education-skill" key={skill}>{skill}</li>)}
-            </ul>
-        </div>
+            {props.coursework && <div className="coursework-display">
+                <p>Relevant Coursework:</p>
+                <ul className="education-coursework">
+                    {props.coursework.map(course => <li className="education-course" key={course}>{course}</li>)}
+                </ul>
+            </div>}
+
+        </section>
     )
 }
--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -4,12 +4,35 @@ import EducationCard from "../components/EducationCard"
 export default function Education() {
 
     const uoPath = "src/assets/university-of-ottawa-vector-logo.svg"
-    const uoSkills = ["Software Engineering", "Mathematics", "Databases"]
+    const uoCourses = ["Algorithms and Databases", "Discrete Structures", "Databases"]
+
+    const ashburyPath = "src/assets/logo.png"
+    const ashburyCourses = ["Intro to Computer Science", "Computer Science"]
+
+    const harvardXPath = "src/assets/harvardX.png"
+
+    const scrimbaPath = "src/assets/KY1XZp9o_400x400.jpg"
+    const scrimbaCourses = ["HTML & CSS", "UI Design", "React", "Advanced React"]
 
     return (<>
         <Navbar active="education" />
         <main className="main-content">
-            <EducationCard title="University of Ottawa" degree="B.Sc. Hons. Computer Science" src={uoPath} alt="University of Ottawa Logo" skills={uoSkills}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus rhoncus varius justo ut tempor. Pellentesque neque ante, semper a iaculis sed, feugiat in purus. Praesent non est molestie, malesuada urna et, consectetur turpis. Nulla ac velit augue. Integer aliquam tortor in urna vestibulum, id rhoncus erat tristique. Aliquam tincidunt tristique odio, lacinia rutrum magna ullamcorper eu. Maecenas tellus metus, pellentesque nec ante sit amet, varius viverra nisl. Fusce orci quam, dapibus quis tortor eu, fringilla ultricies enim. Curabitur lobortis, sapien sit amet vehicula posuere, purus diam vestibulum felis, et hendrerit lorem magna ac justo. Suspendisse potenti. Aenean sagittis nibh quam.</EducationCard>
+            <EducationCard gpa="9.4 / 10" title="University of Ottawa" degree="B.Sc. Hons. Computer Science" src={uoPath} alt="University of Ottawa Logo" coursework={uoCourses}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu neque, auctor ut vestibulum non, consequat eget lectus. Morbi sit amet scelerisque lacus. Etiam a est quis mauris condimentum facilisis. Aenean lacus nibh, sagittis id nibh vel, ornare viverra ipsum. Proin rutrum lacus at mi mattis, id ullamcorper odio finibus.
+
+            </EducationCard>
+            <EducationCard gpa="95%" title="Ashbury College" degree="Ontario Secondary School Diploma" src={ashburyPath} alt="Ashbury College Logo" coursework={ashburyCourses}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu neque, auctor ut vestibulum non, consequat eget lectus. Morbi sit amet scelerisque lacus. Etiam a est quis mauris condimentum facilisis. Aenean lacus nibh, sagittis id nibh vel, ornare viverra ipsum. Proin rutrum lacus at mi mattis, id ullamcorper odio finibus.
+
+            </EducationCard>
+            <EducationCard title="HarvardX" src={harvardXPath} alt="Harvard and EdX Logos" degree="CS50">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu neque, auctor ut vestibulum non, consequat eget lectus. Morbi sit amet scelerisque lacus. Etiam a est quis mauris condimentum facilisis. Aenean lacus nibh, sagittis id nibh vel, ornare viverra ipsum. Proin rutrum lacus at mi mattis, id ullamcorper odio finibus.
+
+            </EducationCard>
+            <EducationCard title="Scrimba" src={scrimbaPath} alt="Scrima Logo" degree="Courses" coursework={scrimbaCourses}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu neque, auctor ut vestibulum non, consequat eget lectus. Morbi sit amet scelerisque lacus. Etiam a est quis mauris condimentum facilisis. Aenean lacus nibh, sagittis id nibh vel, ornare viverra ipsum. Proin rutrum lacus at mi mattis, id ullamcorper odio finibus.
+
+            </EducationCard>
         </main>
     </>)
 }
