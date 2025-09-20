f455db952e31ada39c11cb7a0e639a03b871421a
Wuhib Mezemir <86829490+wuhibm@users.noreply.github.com>
2025-09-20 14:42:56 -0400

    fixed resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import "../styles/Navbar.css"
 import { NavLink } from "react-router";
 import { FaDownload } from "react-icons/fa";
+import resume from "../assets/Resume.pdf"
 
 
 export default function Navbar(props) {
@@ -10,7 +11,7 @@ export default function Navbar(props) {
                 <NavLink to="/" className={props.active === "home" ? "active" : "nav-link"}>Home</NavLink>
                 <NavLink to="/projects" className={props.active === "projects" ? "active" : "nav-link"}>Projects</NavLink>
                 <NavLink to="/education" className={props.active === "education" ? "active" : "nav-link"}>Education</NavLink>
-                <a href="src/assets/Resume.pdf" className="nav-link" download={true}> <FaDownload title='Download Icon' /> Resume</a>
+                <a href={resume} className="nav-link" download={true}> <FaDownload title='Download Icon' /> Resume</a>
             </nav>
         </header>)
 }
